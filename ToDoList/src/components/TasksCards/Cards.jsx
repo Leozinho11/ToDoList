d8322ec07058acf53d5472dfc012bcd5444f91ce
Leozinho11 <leoferraribarreto@hotmail.com>
2022-08-23 12:04:52 -0300

    Card style almost done

--- ToDoList/src/components/TasksCards/Cards.jsx
+++ ToDoList/src/components/TasksCards/Cards.jsx
@@ -4,7 +4,7 @@ export function Cards () {
     return (
         <div className={styles.cards}>
           <input type="checkbox" id="task1" />
-          <label htmlFor="task1">bahsbshasbhlsla</label>
+         <label htmlFor='task1'>bahsbshasbhlsla</label>
         </div>
     )
 }
