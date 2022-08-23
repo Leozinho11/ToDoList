a442485297e2cdf6676d38eedb7a3546910b6364
Leozinho11 <leoferraribarreto@hotmail.com>
2022-08-23 18:50:13 -0300

    Start to make funcionalities

--- ToDoList/src/App.jsx
+++ ToDoList/src/App.jsx
@@ -1,37 +1,86 @@
-import { useState } from 'react'
 import styles from './App.module.css'
-import {PlusCircle} from 'phosphor-react'
+import { useState } from 'react'
 import { Header } from './components/Header/Header'
-import { Task } from './components/Task/Task';
+import { v4 as uuidv4 } from 'uuid'
+import { Cards } from '../TasksCards/Cards';
+import { useState } from 'react';
+import { PlusCircle } from 'phosphor-react';
+
+const task = [
+  {
+      id: uuidv4(),
+      content: '',
+      isChecked: false
+  }
+]
+
+const [tasks, setTasks] = useState([]);
+const [newTaskContent, setNewTaskContent] = useState('');
+
+function handleCreateNewTask(){
+  event.preventDefault()
+
+  setTasks([...tasks, newTaskContent]);
+  setNewTaskContent('');
+}
+
+function handleTaskContentChange(){
+  setNewTaskContent(event.target.value);
+} 
+
+function deleteTask(taskToDelete){
+  const tasksWithoutDeletedOne = tasks.filter(task => {
+      return task !== taskToDelete
+  })
+  setTasks(tasksWithoutDeletedOne);
+}
 
  export function App() {
-
-  const [task, setTask] = useState([]);
-  const [newTask, setNewTask] = useState('');
-
-  function handleNewTask () {
-      event.preventDefault()
-
-      setTask([...task, newTask]);
-      
-  }
 
   return (
     <div className="App">
       <Header />
+        <div>
+            <form className={styles.form} onSubmit={handleNewTask}>
+                <input 
+                type="text" 
+                placeholder='Adicione uma nova tarefa' 
+                />
 
-      <form className={styles.form} onSubmit={handleNewTask}>
-        <input type="text" placeholder='Adicione uma nova tarefa' />
-        <button type='submit'>
-            Criar 
-            <PlusCircle size={20}/>
-        </button>
-      </form>
+                <button type='submit'>
+                     Criar 
+                     <PlusCircle size={20}/>
+                </button>
+            </form>
 
-      <Task />
+            <div className={styles.task}>
 
+                <header className={styles.taskInfo}>
+                    <p className={styles.createdTasksLabel}>
+                    Tarefas Criadas <span className={styles.createdTasksCounter}>5</span>
+                    </p>
+
+                     <p className={styles.completedTasksLabel}>
+                    Concluídas<span className={styles.completedTasksCounter}>2 de 5</span>
+                    </p>
+                </header>
+
+                 <div className={styles.taskBox}>
+                    <div className={styles.taskContent}>
+                        {task.map (tasks =>{
+                            return(
+                                <Cards 
+                                key={tasks.id}
+                                content={tasks.content}
+                                isChecked={tasks.isChecked}
+                                onDeleteTask={deleteTask}
+                                />
+                            )
+                        })}
+                    </div>
+                 </div>
+            </div>
+        </div>
     </div>
   )
 }
-
-
--- ToDoList/src/components/TasksCards/Cards.jsx
+++ ToDoList/src/components/TasksCards/Cards.jsx
@@ -1,13 +1,20 @@
 import styles from './Cards.module.css';
-import { Trash } from 'phosphor-react'
+import { Trash } from 'phosphor-react';
 
-export function Cards () {
+
+export function Cards (content, onDeleteTask, id) {
+
+  function handleDeleteTask (){
+    onDeleteTask(content);
+  }
+  
+
     return (
         <div className={styles.cards}>
           <div className={styles.cardsContent}>
-            <input type="checkbox" id="task1" />
-            <label htmlFor='task1'>bahsbshasbhlsla</label>
-            <button>
+            <input type="checkbox" id={id} />
+            <label htmlFor={id}>{content}</label>
+            <button onClick={handleDeleteTask}>
               <Trash size={24} />
             </button>
           </div>
